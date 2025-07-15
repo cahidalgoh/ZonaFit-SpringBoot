fae28a56e18c1569d1f1d02451c4ae010490dd7c
Carlos Hidalgo <carlos.hidalgoh@hotmail.com>
2025-07-15 15:59:11 -0500

    Initial commit with Springboot

---
+++ src/main/java/com/hidalgocarlos/ZonaFitSpringBoot/ZonaFitSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.hidalgocarlos.ZonaFitSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZonaFitSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZonaFitSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hidalgocarlos/ZonaFitSpringBoot/ZonaFitSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hidalgocarlos.ZonaFitSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZonaFitSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
